21b487164522bb3e6c482a9af6845f510c531ee3
adbiggy <mirabelle.adrien@sfr.fr>
2023-10-19 11:36:47 +0200

    Setup Topic Kafka + Script Python

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
-
 from googleapiclient.discovery import build
+from confluent_kafka import Producer
 
 # Connexion à l'API YouTube Data
 load_dotenv()
@@ -9,6 +9,9 @@ API_KEY = os.getenv("API_KEY")
 
 # Créez une instance de l'API YouTube Data
 youtube = build("youtube", "v3", developerKey=API_KEY)
+
+# Configuration du producteur Kafka
+producer = Producer({'bootstrap.servers': 'kafka-node:9092'})
 
 # Utilisez la méthode search.list pour obtenir les dernières vidéos
 search_response = youtube.search().list(
@@ -19,8 +22,7 @@ search_response = youtube.search().list(
     order="date"  # Triez par date pour obtenir les vidéos les plus récentes
 ).execute()
 
-
-# Parcourez les résultats et imprimez les titres des vidéos
+# Parcourez les résultats et publiez les titres des vidéos dans le sujet Kafka
 for search_result in search_response.get("items", []):
     if 'videoId' in search_result['id']:
         video_id = search_result['id']['videoId']
@@ -28,4 +30,9 @@ for search_result in search_response.get("items", []):
         if 'items' in video_info:
             video = video_info['items'][0]
             title = video['snippet']['title']
-            print("Titre de la vidéo :", title)
+            # Publiez le titre dans le sujet Kafka
+            producer.produce('youtube-titles', key=video_id, value=title)
+            print("Titre de la vidéo publié dans Kafka :", title)
+
+# Assurez-vous de délivrer les messages au cluster Kafka
+producer.flush()
